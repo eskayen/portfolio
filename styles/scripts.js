fc543dc1d700215a7bc3533571ea8df358295cec
Saad <niaziksaad@gmail.com>
2022-09-25 19:51:44 -0400

    alignment fix

--- styles/scripts.js
+++ styles/scripts.js
@@ -2,9 +2,6 @@
 
 var root = document.querySelector(':root');
 var rootStyles = root.getComputedStyle(root);
-var currentAccent =  rootStyles.getPropertyValue('--accent');
-console.log(currentAccent);
-
 
 function colorChange(){
 	root.style.setProperty('--accent','blue');
