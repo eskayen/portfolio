aa54c0d6739acba06c3a1714fd6b18ce4ab66c8f
Saad <niaziksaad@gmail.com>
2022-09-26 00:04:49 -0400

    theme change dropdown

--- styles/scripts.js
+++ styles/scripts.js
@@ -8,3 +8,21 @@ function colorChangeToDark(){
 	root.style.setProperty('--text','#F2F2F2');
 	root.style.setProperty('--accent',"#5CB8E4");
 }
+
+function dropdownFunction(){
+	document.getElementById("colorlist").classList.toggle("show");
+}
+
+window.onClick = function(event){
+	if(!event.target.matches('.colorButton'))
+	{
+		var list = document.getElementsByClassName("dropdownContent");
+		var i;
+		for(i = 0; i < list.length; i++){
+			var open = list[i];
+			if(open.classList.contains('show')){
+				open.classList.remove('show');
+			}
+		}
+	}
+}
